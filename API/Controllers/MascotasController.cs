bc289d4e192d66bf1311d5a6f7ccb904657c6027
uricherni <uricherni@DESKTOP-43PO4QG>
2022-05-13 21:12:16 -0300

    Agregar archivos de proyecto.

---
+++ API/Controllers/MascotasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class MascotasController : ApiController
+    {
+        private ApiEntities db = new ApiEntities();
+
+        // GET: api/Mascotas
+        public IQueryable<Mascotas> GetMascotas()
+        {
+            return db.Mascotas;
+        }
+
+        // GET: api/Mascotas/5
+        [ResponseType(typeof(Mascotas))]
+        public IHttpActionResult GetMascotas(int id)
+        {
+            Mascotas mascotas = db.Mascotas.Find(id);
+            if (mascotas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mascotas);
+        }
+
+        // PUT: api/Mascotas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMascotas(int id, Mascotas mascotas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != mascotas.IdMascotas)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(mascotas).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MascotasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Mascotas
+        [ResponseType(typeof(Mascotas))]
+        public IHttpActionResult PostMascotas(Mascotas mascotas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Mascotas.Add(mascotas);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = mascotas.IdMascotas }, mascotas);
+        }
+
+        // DELETE: api/Mascotas/5
+        [ResponseType(typeof(Mascotas))]
+        public IHttpActionResult DeleteMascotas(int id)
+        {
+            Mascotas mascotas = db.Mascotas.Find(id);
+            if (mascotas == null)
+            {
+                return NotFound();
+            }
+
+            db.Mascotas.Remove(mascotas);
+            db.SaveChanges();
+
+            return Ok(mascotas);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MascotasExists(int id)
+        {
+            return db.Mascotas.Count(e => e.IdMascotas == id) > 0;
+        }
+    }
+}
---
+++ API/Controllers/RefugiosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class RefugiosController : ApiController
+    {
+        private ApiEntities db = new ApiEntities();
+
+        // GET: api/Refugios
+        public IQueryable<Refugios> GetRefugios()
+        {
+            return db.Refugios;
+        }
+
+        // GET: api/Refugios/5
+        [ResponseType(typeof(Refugios))]
+        public IHttpActionResult GetRefugios(int id)
+        {
+            Refugios refugios = db.Refugios.Find(id);
+            if (refugios == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(refugios);
+        }
+
+        // PUT: api/Refugios/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRefugios(int id, Refugios refugios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != refugios.IdRefugios)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(refugios).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RefugiosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Refugios
+        [ResponseType(typeof(Refugios))]
+        public IHttpActionResult PostRefugios(Refugios refugios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Refugios.Add(refugios);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = refugios.IdRefugios }, refugios);
+        }
+
+        // DELETE: api/Refugios/5
+        [ResponseType(typeof(Refugios))]
+        public IHttpActionResult DeleteRefugios(int id)
+        {
+            Refugios refugios = db.Refugios.Find(id);
+            if (refugios == null)
+            {
+                return NotFound();
+            }
+
+            db.Refugios.Remove(refugios);
+            db.SaveChanges();
+
+            return Ok(refugios);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RefugiosExists(int id)
+        {
+            return db.Refugios.Count(e => e.IdRefugios == id) > 0;
+        }
+    }
+}
